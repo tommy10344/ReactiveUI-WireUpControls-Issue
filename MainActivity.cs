57e88cae6b5dfae2952711f49808431f7d293afa
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2024-01-15 11:27:45 +0900

    Add calling WireUpControls (Reproduce issue)

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,13 +1,23 @@
+using ReactiveUI;
+
 namespace ReactiveUI_WireUpControls_Issue;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
-public class MainActivity : Activity
+public class MainActivity : ReactiveActivity
 {
+
+    [WireUpResource("app_text")]
+    public TextView AppText { get; private set; }
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
+
+        this.WireUpControls(ControlFetcherMixin.ResolveStrategy.ExplicitOptIn);
+
+        AppText.SetText("Hello, ReactiveUI!", TextView.BufferType.Normal);
     }
 }
